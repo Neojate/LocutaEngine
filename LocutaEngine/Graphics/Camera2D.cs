0d9743bdcc07c866bfc277463d2ec52fce9125bc
Albert Alonso <albert15_89@hotmail.com>
2022-05-09 23:58:32 +0200

    graphics

---
+++ LocutaEngine/Graphics/Camera2D.cs
@@ -0,0 +1,89 @@
+﻿using LocutaEngine.Inputs;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace LocutaEngine.Graphics
+{
+    public class Camera2D
+    {
+        public Keys MoveUpKey = Keys.Up;
+        public Keys MoveDownKey = Keys.Down;
+        public Keys MoveLeftKey = Keys.Left;
+        public Keys MoveRightKey = Keys.Right;
+
+        private Input input;
+
+        private Canvas canvas;
+
+        private float z;
+        private Matrix transform;
+        private Vector2 position;
+        private float rotation;
+
+        public Matrix Transform { get { return transform; } }
+        public Vector2 Position { get { return position; } }
+        public float Rotation { get { return rotation; } }
+
+        public Camera2D(Canvas canvas)
+        {
+            input = Input.Instance;
+            this.canvas = canvas;
+
+            z = 1f;
+            rotation = 0f;
+            position = Vector2.Zero;
+
+            UpdateMatrix();
+        }
+
+        public void Move(Vector2 amount)
+        {
+            position += amount;
+            UpdateMatrix();
+        }
+
+        public void Zoom(float amount)
+        {
+            z += amount;
+            if (z < 0.1f)
+                z = 0.1f;
+
+            UpdateMatrix();
+        }
+
+        public void Rotate(float amount)
+        {
+            rotation += amount;
+            UpdateMatrix();
+        }
+
+        public void UpdateMatrix()
+        {
+            transform = Matrix.CreateTranslation(new Vector3(-position.X, -position.Y, 0)) *
+                Matrix.CreateRotationZ(rotation) *
+                Matrix.CreateScale(new Vector3(z, z, 1)) *
+                Matrix.CreateTranslation(new Vector3(canvas.Width * 0.5f, canvas.Height * 0.5f, 0));
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (input.IsKeyDown(MoveUpKey))
+                Move(new Vector2(0, -10));
+            if (input.IsKeyDown(MoveDownKey))
+                Move(new Vector2(0, 10));
+            if (input.IsKeyDown(MoveLeftKey))
+                Move(new Vector2(-10, 0));
+            if (input.IsKeyDown(MoveRightKey))
+                Move(new Vector2(10, 0));
+            if (input.IsWheelDown())
+                Zoom(-0.1f);
+            if (input.IsWheelUp())
+                Zoom(0.1f);
+            if (input.IsKeyDown(Keys.R))
+                Rotate(0.1f);
+
+        }
+
+    }
+}
---
+++ LocutaEngine/Graphics/Canvas.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LocutaEngine.Graphics
+{
+    public class Canvas
+    {
+        private RenderTarget2D renderTarget2D;
+
+        private Game game;
+
+        public int Width { get { return renderTarget2D.Width; } }
+        public int Height { get { return renderTarget2D.Height; } }
+        public Vector2 Center { get { return new Vector2(Width, Height) * 0.5f; } }
+
+        public Game Game { get { return game; } }
+
+        public Canvas(Game game, int width, int height)
+        {
+            this.game = game;
+
+            renderTarget2D = new RenderTarget2D(game.GraphicsDevice, width, height);
+        }
+
+        public void Set()
+        {
+            game.GraphicsDevice.SetRenderTarget(renderTarget2D);
+        }
+
+        public void Unset()
+        {
+            game.GraphicsDevice.SetRenderTarget(null);
+        }
+
+        public void Present(SpriteBatch spriteBatch)
+        {
+            game.GraphicsDevice.Clear(Color.Pink);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(renderTarget2D, CalculateDestinationRectangle(), Color.White);
+            spriteBatch.End();
+        }
+
+        public Rectangle CalculateDestinationRectangle()
+        {
+            Rectangle backbufferBounds = game.GraphicsDevice.PresentationParameters.Bounds;
+            float backBufferAspectRatio = (float)backbufferBounds.Width / backbufferBounds.Height;
+            float screenAspectRatio = (float)Width / Height;
+
+            float rx = 0f;
+            float ry = 0f;
+            float rw = backbufferBounds.Width;
+            float rh = backbufferBounds.Height;
+
+            if (screenAspectRatio > backBufferAspectRatio)
+            {
+                rh = rw / screenAspectRatio;
+                ry = (backbufferBounds.Height - rh) / 2f;
+            }
+            else if (screenAspectRatio < backBufferAspectRatio)
+            {
+                rw = rh * screenAspectRatio;
+                rx = backbufferBounds.Width / 2f;
+            }
+
+            return new Rectangle((int)rx, (int)ry, (int)rw, (int)rh);
+        }
+    }
+}
