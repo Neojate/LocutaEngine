d454e7c5cdb65331a6ca61dd28ad99d017da73ff
Albert Alonso <albert15_89@hotmail.com>
2022-07-08 19:08:35 +0200

    Nuevos Componentes

---
+++ LocutaEngine/Ecs/SpriteRenderer.cs
@@ -0,0 +1,54 @@
+﻿using LocutaEngine.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LocutaEngine.Ecs
+{
+    public class SpriteRenderer : Component
+    {
+        //Textura de la imagen
+        private Texture2D texture;
+
+        //SourceRectangle de la imagen
+        private Rectangle sourceRectangle;
+
+        //Camara para el dibujado
+        private Camera2D camera;
+
+        //Instancia del componente transform
+        private Transform transform;
+
+        public SpriteRenderer(Texture2D texture, Rectangle sourceRectangle, Camera2D camera)
+        {
+            this.texture = texture;
+            this.sourceRectangle = sourceRectangle;
+            this.camera = camera;
+            
+            transform = entity.GetComponent<Transform>();
+        }
+
+        public void Render(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Begin(
+                sortMode: SpriteSortMode.Immediate,
+                blendState: BlendState.AlphaBlend,
+                samplerState: SamplerState.PointClamp,
+                rasterizerState: RasterizerState.CullNone,
+                transformMatrix: camera == null ? Matrix.Identity : camera.Transform
+            );
+
+            spriteBatch.Draw(
+                texture: texture,
+                destinationRectangle: new Rectangle((int)transform.Position.X, (int)transform.Position.Y, (int)transform.Size.X, (int)transform.Size.Y),
+                sourceRectangle: sourceRectangle,
+                color: Color.White,
+                rotation: transform.Rotation,
+                origin: Vector2.Zero,
+                effects: SpriteEffects.None,
+                layerDepth: 0f
+            );
+
+            spriteBatch.End();
+        }
+    }
+}
---
+++ LocutaEngine/Ecs/Transform.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace LocutaEngine.Ecs
+{
+    public class Transform : Component
+    {
+        //Posición de la entidad en esquina superior izquierda
+        private Vector2 position;
+
+        //Tamaño de la entidad
+        private Vector2 size;
+
+        //Rotación de la entidad
+        private float rotation;
+
+        public Vector2 Position { get { return position; } set { position = value; } }
+
+        public Vector2 Size { get { return size; } set { size = value; } }
+
+        public float Rotation { get { return rotation; } set { rotation = value; } }
+
+        public Vector2 Center { get { return position + size * 0.5f; } }
+    }
+}
